adda9aa73850e489a9971fc1722569716760835d
Lera Demina <lera.demia.12@mail.ru>
2023-12-14 19:31:17 +0300

    add: lab-5

--- js/addEventListener.js
+++ js/addEventListener.js
@@ -6,3 +6,36 @@
         document.body.appendChild(loadTimeElement);
     });
 })();
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Получаем текущий путь страницы
+    var currentPath = window.location.pathname;
+
+    // Находим все кнопки меню
+    var menuButtons = document.querySelectorAll(".button");
+
+    // Проходим по всем кнопкам меню
+    menuButtons.forEach(function (button) {
+        // Получаем путь из атрибута data-path
+        var buttonPath = button.getAttribute("data-path");
+
+        // Проверяем, соответствует ли текущий путь кнопке меню
+        if (currentPath === buttonPath) {
+            button.classList.add("active");
+        } else {
+            button.classList.add("inactive");
+        }
+
+        // Добавляем обработчик события для состояния hover
+        button.addEventListener("mouseover", function () {
+            button.classList.remove("inactive");
+        });
+
+        // Добавляем обработчик события для состояния hover
+        button.addEventListener("mouseout", function () {
+            if (currentPath !== buttonPath) {
+                button.classList.add("inactive");
+            }
+        });
+    });
+});
--- js/script.js
+++ js/script.js
@@ -3,3 +3,4 @@ let button = document.querySelector('.h2');
 button.onclick = function() {
     document.location='womenSide.html'
 }
+
